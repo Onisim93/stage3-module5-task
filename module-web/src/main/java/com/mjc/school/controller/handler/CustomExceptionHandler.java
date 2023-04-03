af4c9a64211e34aa389bcbb4c41c7583f44f7068
onisim_albarov <onisim93@gmail.com>
2023-04-03 20:37:04 +0400

    added exception handler to web layer

---
+++ module-web/src/main/java/com/mjc/school/controller/handler/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.mjc.school.controller.handler;
+
+import com.mjc.school.service.exception.NoSuchEntityException;
+import com.mjc.school.service.exception.ValidatorException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(NoSuchEntityException.class)
+    public ResponseEntity<ErrorMessage> noSuchEntityException(NoSuchEntityException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler(ValidatorException.class)
+    public ResponseEntity<ErrorMessage> validationException(ValidatorException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(new ErrorMessage(e.getMessage()));
+    }
+}
---
+++ module-web/src/main/java/com/mjc/school/controller/handler/ErrorMessage.java
@@ -0,0 +1,10 @@
+package com.mjc.school.controller.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessage {
+    private String message;
+}
